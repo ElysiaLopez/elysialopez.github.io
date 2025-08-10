97adaa188270fabe70bca23afc4394de4383bf17
Elysia Lopez <elysialopez528@gmail.com>
2025-08-10 16:22:26 -0400

    do i sound less like a robot yet

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,63 +35,87 @@ class Header extends Component {
     }, (props, prevProp) => true);
 
     return (
-      <header id="home" style={{ height: window.innerHeight - 140, display: 'block' }}>
-        <div className="row aligner" style={{height: '100%'}}>
-          <div className="col-md-12">
-            <div>
-              <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
-              <br/>
-              <h1 className="mb-0">
-                <Typical steps={[name]} wrapper="p" />
-              </h1>
-              <div className="title-container">
-                <HeaderTitleTypeAnimation />
-              </div>
-              
-              <Switch
-                checked={this.state.checked}
-                onChange={this.onThemeSwitchChange}
-                offColor="#baaa80"
-                onColor="#353535"
-                className="react-switch mx-auto"
-                width={90}
-                height={40}
-                uncheckedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="twemoji:owl"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "20px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                checkedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="noto-v1:sun-with-face"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "10px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                id="icon-switch"
-              />
-            </div>
-          </div>
+      <header
+  id="home"
+  style={{
+    height: window.innerHeight - 140,
+    display: "block",
+    position: "relative"
+  }}
+>
+  <span
+    style={{
+      position: "absolute",
+      top: "10px",
+      right: "10px",
+      fontSize: "2rem",
+      color: "#333"
+    }}
+  >
+    [TODO: make navigation bar]
+  </span>
+
+  <div className="row aligner" style={{ height: "100%" }}>
+    <div className="col-md-12">
+      <div>
+        <span
+          className="iconify header-icon"
+          data-icon="la:laptop-code"
+          data-inline="false"
+        ></span>
+        <br />
+        <h1 className="mb-0">
+          <Typical steps={[name]} wrapper="p" />
+        </h1>
+        <div className="title-container">
+          <HeaderTitleTypeAnimation />
         </div>
-      </header>
+
+        <Switch
+          checked={this.state.checked}
+          onChange={this.onThemeSwitchChange}
+          offColor="#baaa80"
+          onColor="#353535"
+          className="react-switch mx-auto"
+          width={90}
+          height={40}
+          uncheckedIcon={
+            <span
+              className="iconify"
+              data-icon="twemoji:owl"
+              data-inline="false"
+              style={{
+                display: "block",
+                height: "100%",
+                fontSize: 25,
+                textAlign: "end",
+                marginLeft: "20px",
+                color: "#353239",
+              }}
+            ></span>
+          }
+          checkedIcon={
+            <span
+              className="iconify"
+              data-icon="noto-v1:sun-with-face"
+              data-inline="false"
+              style={{
+                display: "block",
+                height: "100%",
+                fontSize: 25,
+                textAlign: "end",
+                marginLeft: "10px",
+                color: "#353239",
+              }}
+            ></span>
+          }
+          id="icon-switch"
+        />
+      </div>
+    </div>
+  </div>
+</header>
+
     );
   }
 }
